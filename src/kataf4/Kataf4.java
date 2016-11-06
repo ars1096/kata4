a7de90cce62d00bb434a98f4455a281b4230b83f
Adrian <Adrian@192.168.2.9>
2016-11-06 17:57:23 +0000

    kata4 v1

---
+++ src/kataf4/Kataf4.java
@@ -0,0 +1,19 @@
+
+
+package kataf4;
+
+import java.io.File;
+
+
+public class Kataf4 {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\");
+        String[] names = file.list();
+        for (String name : names) {
+            System.out.println(name);
+        }
+            
+    }
+    
+}
