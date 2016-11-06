0d3e6f1568e91d8c0ce630cc67506463c5995707
Adrian <Adrian@192.168.2.9>
2016-11-06 19:11:24 +0000

    kata4 v1

---
+++ src/kataf4/MailHistogramBuilder.java
@@ -0,0 +1,14 @@
+
+package kataf4;
+import java.util.ArrayList;
+
+public class MailHistogramBuilder {
+   public static Histogram <String> builder(ArrayList<String> mail){
+   Histogram histo= new Histogram<>();
+       for (String mail1 : mail) {
+           histo.increment(new Mail(mail1).getDomain());
+           
+       }
+       return histo;
+   } 
+}
